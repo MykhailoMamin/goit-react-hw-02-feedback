1ccceb008c99938002c7891d473bf7fd6e80290f
MykhailoMamin <mishamamin2016@gmail.com>
2023-10-23 21:11:05 +0300

    fix section

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,19 +1,22 @@
 import css from './FeedbackOptions.module.css';
+import { Section } from './Section';
 
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return (
-    <ul className={css.btnList}>
-      {options.map((option, index) => (
-        <li key={index}>
-          <button
-            className={css.feedbackBtb}
-            id={option}
-            onClick={e => onLeaveFeedback(e)}
-          >
-            {option}
-          </button>
-        </li>
-      ))}
-    </ul>
+    <Section title="Feedback Options">
+      <ul className={css.btnList}>
+        {options.map((option, index) => (
+          <li key={index}>
+            <button
+              className={css.feedbackBtb}
+              id={option}
+              onClick={() => onLeaveFeedback(option)}
+            >
+              {option}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </Section>
   );
 };
